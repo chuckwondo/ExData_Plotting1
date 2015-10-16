c72476ba19e569b3cdfa08db7f65fb083eb9176d
Chuck Daniels <cjdaniels4@gmail.com>
2015-10-15 21:40:02 -0400

    Simplified code for plot 4.

--- plot4.R
+++ plot4.R
@@ -3,8 +3,6 @@ library(sqldf)
 df <- read.csv.sql("household_power_consumption.txt", header = TRUE, sep = ';',
                    sql = "select * from file where Date in ('1/2/2007', '2/2/2007')")
 df["Date_time"] <- list(strptime(paste(df$Date, df$Time), "%d/%m/%Y %H:%M:%S"))
-Sub_meterings <- c(df$Sub_metering_1, df$Sub_metering_2, df$Sub_metering_3)
-Date_times <- c(df$Date_time, df$Date_time, df$Date_time)
 
 par(mfrow = c(2, 2), cex = 0.65)
 
@@ -12,11 +10,10 @@ with(df, {
   plot(Date_time, Global_active_power, type = "l", xlab = "",
        ylab = "Global Active Power")
   plot(Date_time, Voltage, type = "l", xlab = "datetime")
-  plot(Date_times, Sub_meterings, type = "n", xlab = "",
-    ylab = "Energy sub metering")
-  with(df, lines(Date_time, Sub_metering_1, col = "black"))
-  with(df, lines(Date_time, Sub_metering_2, col = "red"))
-  with(df, lines(Date_time, Sub_metering_3, col = "blue"))
+  plot(Date_time, Sub_metering_1, type = "l", col = "black", xlab = "",
+       ylab = "Energy sub metering")
+  lines(Date_time, Sub_metering_2, col = "red")
+  lines(Date_time, Sub_metering_3, col = "blue")
   legend("topright", legend = paste0("Sub_metering_", 1:3), lwd = 1,
          col = c("black", "red", "blue"), bty = "n")
   plot(Date_time, Global_reactive_power, type = "l", xlab = "datetime")
